ff57197d7a3df22b4427af6b0e9eaabdcfac9a18
Sinvaldo Pissinatti <112958318+SinvaldoPissinatti@users.noreply.github.com>
2023-01-24 17:19:55 -0300

    Add files via upload

--- Aula05/js/script05.js
+++ Aula05/js/script05.js
@@ -29,3 +29,24 @@ pessoa.altura;
 pessoa['nome'];
 pessoa['idade'];
 pessoa['altura'];
+ 
+//OPERACAO COM OBJETO, IMC = peso / (altura * altura)
+let imc = pessoa.peso / (pessoa.altura * pessoa.altura);
+
+//.toFixed(X)
+// limita o numero de casas decimais
+console.log("IMC "+ imc.toFixed(2));
+
+//ALTERAR/ATUALIZAR VALOR de propriedade
+pessoa.nome = "Sinvaldo Pissinatti";
+produtos.descricao = ['Arroz'];
+produtos.preco = [4.99];
+
+//USANDO SPREED OPERATOR   
+produtos.descricao = [...produtos.descricao, 'Feijão', 'Trigo'];
+produtos.preco = [...produtos.preco, 5.99, 6.00];
+
+//USANDO SPREED OPERATOR em constantes
+carros.marca = [...carros.marca, 'VW'];
+carros.modelo = [...carros.modelo, 'Fusca'];
+carros.ano = [...carros.ano, '1979'];
